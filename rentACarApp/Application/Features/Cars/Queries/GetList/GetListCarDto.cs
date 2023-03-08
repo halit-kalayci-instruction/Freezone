dd4b0d8ac7c77101605a7a6e0b253afe2656785c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-03-08 11:52:01 +0300

    Dto Changes

--- rentACarApp/Application/Features/Cars/Queries/GetList/GetListCarDto.cs
+++ rentACarApp/Application/Features/Cars/Queries/GetList/GetListCarDto.cs
@@ -8,7 +8,10 @@ namespace Application.Features.Cars.Queries.GetList
     {
         public int Id { get; set; }
         public string BrandName { get; set; }
+        public int Kilometer { get; set; }
         public string ModelName { get; set; }
+        public short MinFindeksCreditRate { get; set; }
+
         public string Plate { get; set; }
         public CarState CarState { get; set; }
         public int ModelYear { get; set; }
