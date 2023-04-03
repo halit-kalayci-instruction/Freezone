592e484fc53c328d60ef7484b406b3a708291272
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-03 11:33:15 +0300

    OnConnected & OnDisconnected

--- rentACarApp/Application/Hubs/ChatHub.cs
+++ rentACarApp/Application/Hubs/ChatHub.cs
@@ -9,10 +9,25 @@ namespace Application.Hubs
 {
     public class ChatHub : Hub
     {
+        static List<string> connectedIds = new List<string>();
         public async Task SendMessageAsync(string message)
         {
             // Fonksiyon
-            await Clients.All.SendAsync("ReceiveMessage",message);
+            await Clients.Others.SendAsync("ReceiveMessage",message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            connectedIds.Add(Context.ConnectionId);
+            Clients.All.SendAsync("UserListChanged", connectedIds);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            connectedIds.Remove(Context.ConnectionId);
+            Clients.All.SendAsync("UserListChanged", connectedIds);
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
