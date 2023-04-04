5c1eb7f2a7b3a9937913ecba03a15c7a576d727a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-04 09:59:59 +0300

    SignalR Client Types

--- rentACarApp/Application/Hubs/ChatHub.cs
+++ rentACarApp/Application/Hubs/ChatHub.cs
@@ -22,10 +22,30 @@ namespace Application.Hubs
         static List<string> connectedIds = new List<string>();
 
         static List<string> connectedUsers = new List<string>();
+
+        readonly List<string> blockedUsers = new List<string>() { "vj123lcsd", "jbas81234" };
         public async Task SendMessageAsync(string message)
         {
-            // Fonksiyon
+            // Gönderen hariç tüm bağlı clientlara erişim.
+            #region Others 
             await Clients.Others.SendAsync("ReceiveMessage",message);
+            #endregion
+            // Bağlı tüm clientlara erişim.
+            #region All
+            await Clients.All.SendAsync("ReceiveMessage", message);
+            await Clients.AllExcept(blockedUsers).SendAsync("ReceiveMessage", message);
+            #endregion
+            // İsteği triggerlayan client'a erişim
+            #region Caller
+            await Clients.Caller.SendAsync("ReceiveMessage", message);
+            #endregion
+            // Önceden grupladığım ve o an ilgili grup bağlantısına sahip tüm kullanıcılara erişim.
+            #region Group
+            await Clients.Group("IKGrubu").SendAsync("ReceiveMessage", message);
+            await Clients.OthersInGroup("IKGrubu").SendAsync("ReceiveMessage", message);
+            // ["abc123","bca321","vj123lcsd", "jbas81234"]
+            await Clients.GroupExcept("IKGrubu", blockedUsers).SendAsync("ReceiveMessage", message);
+            #endregion
         }
 
         public override Task OnConnectedAsync()
@@ -35,6 +55,16 @@ namespace Application.Hubs
             connectedIds.Add(Context.ConnectionId);
             connectedUsers.Add(user.Identity.Name);
             Clients.All.SendAsync("UserListChanged", connectedUsers);
+
+            // Claims.Groups = ["IKGrubu", "Yönetim"]
+
+
+
+            #region Group İşlemleri
+            // Kullanıcının TITLE_DEFINITON alanındaki Title alanı grup ismi olmalıdır.
+            Groups.AddToGroupAsync(Context.ConnectionId, "IKGrubu");
+            #endregion
+
             return base.OnConnectedAsync();
         }
 
@@ -44,6 +74,8 @@ namespace Application.Hubs
             connectedIds.Remove(Context.ConnectionId);
             connectedUsers.Remove(user.Identity.Name);
             Clients.All.SendAsync("UserListChanged", connectedUsers);
+
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, "IKGrubu");
             return base.OnDisconnectedAsync(exception);
         }
     }
